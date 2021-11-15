452a38c0ef875bcddf7ebb0c3a548b2975599d1c
/shynox <66389292+BilalTaner@users.noreply.github.com>
2021-11-15 20:34:02 +0300

    Fixing waiting times and some regulations...

--- InstaHooker.js
+++ InstaHooker.js
@@ -7,7 +7,7 @@ program
     .option("-u, --username <username>", "Username of the targetted instagram user")
     .option("-s, --sid <session_id>", "Session id of the user. Required when hooking private accounts")
     .option("-o, --output <folder>", "Output directory for downloading the media [default: 'instahooker-out']")
-    .option("-t, --timeout <timeout (millisecond)>", "Download time of files [default: 100 millisecond (recommended)]")
+    .option("-t, --timeout <timeout (millisecond)>", "Download time of files [default: 50 millisecond (recommended)]")
     .addHelpText("afterAll", "Example: InstaHooker.exe -u someone -s myCoolSessionID -o myFolder")
     .parse(process.argv);
 
--- index.js
+++ index.js
@@ -1,67 +1,62 @@
 const fs = require("fs");
 const fetch = require("node-fetch");
 
-module.exports.startHook = async (setting = { username: "", cookie: "", timeout: 100, output: "" }) => {
+module.exports.startHook = async (setting = { username: "", cookie: "", timeout: 50, output: "" }) => {
     const options = {
         username: setting.username,
         cookie: `sessionid=${setting.cookie};`,
-        timeout: setting.timeout ? setting.timeout : 100,
+        timeout: setting.timeout ? setting.timeout : 50,
         output: setting.output || "instahooker-out"
     }
-    if (!options.username) return console.log("WARN: You must enter a username!")
 
-    else if (!options.cookie) return console.log("WARN: You must enter a sessionid for hooking images!")
+    const posts = [];
 
-    else {
+    fetch(`https://www.instagram.com/${options.username}/?__a=1`, { headers: { cookie: options.cookie } }).then(async response => {
+        const data = await response.json();
+        if (!data) return console.log("WARN: User not found!");
 
-        const posts = [];
+        const id = data.graphql.user.id,
+            fetchedData = await fetch(`https://www.instagram.com/graphql/query/?query_hash=8c2a529969ee035a5063f2fc8602a0fd&variables={"id":"${id}","first":"5000"}`, { headers: { cookie: options.cookie } }).then(res => res.json()),
+            mediaCount = fetchedData.data.user.edge_owner_to_timeline_media.count;
 
-        fetch(`https://www.instagram.com/${options.username}/?__a=1`, { headers: { cookie: options.cookie } }).then(async response => {
-            const data = await response.json();
-            if (!data) return console.log("WARN: User not found!");
+        if (!mediaCount) return console.log("WARN: There are no posts on the account you entered!");
 
-            const id = data.graphql.user.id,
-                fetchedData = await fetch(`https://www.instagram.com/graphql/query/?query_hash=8c2a529969ee035a5063f2fc8602a0fd&variables={"id":"${id}","first":"5000"}`, { headers: { cookie: options.cookie } }).then(res => res.json()),
-                mediaCount = fetchedData.data.user.edge_owner_to_timeline_media.count;
+        let after = fetchedData.data.user.edge_owner_to_timeline_media.page_info.end_cursor;
 
-            if (!mediaCount) return console.log("WARN: There are no posts on the account you entered!");
+        getData(fetchedData, posts);
 
-            let after = fetchedData.data.user.edge_owner_to_timeline_media.page_info.end_cursor;
-
-            getData(fetchedData, posts);
-
-            console.log("READY: Posts are starting to fetch...");
-            await sleep(1000);
-            process.stdout.write(`\rREADY: Fetcing data from instagram... PROGRESS: ${mediaCount > 50 ? 50 : mediaCount}/${mediaCount} (${posts.length} media)`);
+        console.log("READY: Posts are starting to fetch...");
+        await sleep(500);
+        process.stdout.write(`\rREADY: Fetcing data from instagram... PROGRESS: ${mediaCount > 50 ? 50 : mediaCount}/${mediaCount} (${posts.length} media)`);
 
 
-            if (mediaCount > 50) {
-                for (let i = 0; i < Math.floor(mediaCount / 50); i++) {
+        if (mediaCount > 50) {
+            for (let i = 0; i < Math.floor(mediaCount / 50); i++) {
 
-                    const fetchingMediaFor = await fetch(`https://www.instagram.com/graphql/query/?query_hash=8c2a529969ee035a5063f2fc8602a0fd&variables={"id":"${id}","first":"5000","after":"${after}"}`, { headers: { cookie: options.cookie } })
-                        .then(x => x.json());
+                const fetchingMediaFor = await fetch(`https://www.instagram.com/graphql/query/?query_hash=8c2a529969ee035a5063f2fc8602a0fd&variables={"id":"${id}","first":"5000","after":"${after}"}`, { headers: { cookie: options.cookie } })
+                    .then(x => x.json());
 
-                    getData(fetchingMediaFor, posts);
+                getData(fetchingMediaFor, posts);
 
-                    process.stdout.write(`\rREADY: Fetcing data from instagram... PROGRESS: ${i + 1 >= Math.floor(mediaCount / 50) ? mediaCount : (i + 2) * 50}/${mediaCount} (${posts.length} media)`);
+                process.stdout.write(`\rREADY: Fetcing data from instagram... PROGRESS: ${i + 1 >= Math.floor(mediaCount / 50) ? mediaCount : (i + 2) * 50}/${mediaCount} (${posts.length} media)`);
 
-                    after = fetchingMediaFor.data.user.edge_owner_to_timeline_media.page_info.end_cursor;
-                }
+                after = fetchingMediaFor.data.user.edge_owner_to_timeline_media.page_info.end_cursor;
             }
+        }
 
-            //Check the posts array
-            if (!posts.length && data.graphql.user.is_private) return console.log("WARN: The account you want to reach is private and you don't follow it, so media cannot be downloaded!");
+        //Check the posts array
+        if (!posts.length && data.graphql.user.is_private) return console.log("WARN: The account you want to reach is private and you don't follow it, so media cannot be downloaded!");
 
-            //Show info for download
-            await sleep(1000);
-            console.log("\nREADY: All posts have been fetched, preparing to download.");
-            await sleep(1000);
-            console.log("READY: Download Starting...");
-            await sleep(2000);
-            await getRemoteFile(posts, `./${options.output}/`, options.timeout);
-        })
+        //Show info for download
+        await sleep(800);
+        console.log("\nREADY: All posts have been fetched, preparing to download.");
+        await sleep(800);
+        console.log("READY: Download Starting...");
+        await sleep(1300);
+        await getRemoteFile(posts, `./${options.output}/`, options.timeout);
+    })
 
-    }
+
 }
 
 async function getRemoteFile(PostArray, output, ms) {
@@ -87,13 +82,10 @@ async function getRemoteFile(PostArray, output, ms) {
 
         if (i === PostArray.length) {
             console.log(`\nCOMPLETE: Download finished! Statics: Video Count: ${PostArray.filter(x => x.name.endsWith(".mp4")).length} - Photo Count: ${PostArray.filter(x => x.name.endsWith(".png")).length}`);
-
             //advertisement
             return console.log("> InstaHooker CLI - By Bilal Taner (shynox)\n> Thanks to Tuhana (tuhana) for helping cli\n> Type -h for help menu");
         };
-
     }
-
 }
 
 function getData(fetch, posts) {
