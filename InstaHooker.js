13b0b8ed2ffed57816aacc8b12e83331450c1f2b
/shynox <66389292+BilalTaner@users.noreply.github.com>
2021-11-18 03:29:36 +0300

    Some big changes and added download posts from url :alien:

--- InstaHooker.js
+++ InstaHooker.js
@@ -1,14 +1,16 @@
 const { Command } = require("commander"),
     program = new Command(),
-    { startHook } = require("./index.js")
+    { PostHooker, ProfileHooker } = require("./index.js")
 
 program
     .version(require("./package.json").version)
+    .option("-p, --post <url | shortcode>", "URL or Shortcode of the targetted post (reel, image, video)")
     .option("-u, --username <username>", "Username of the targetted instagram user")
-    .option("-s, --sid <session_id>", "Session id of the user. Required when hooking private accounts")
+    .option("-s, --sid <session_id> (required)", "Session id of the user")
     .option("-o, --output <folder>", "Output directory for downloading the media [default: 'instahooker-out']")
     .option("-t, --timeout <timeout (millisecond)>", "Download time of files [default: 50 millisecond (recommended)]")
     .addHelpText("afterAll", "Example: InstaHooker.exe -u someone -s myCoolSessionID -o myFolder")
+    .addHelpText("afterAll", "Example for Posts: InstaHooker.exe -p postURL/shortcode -s myCoolSessionID -o myFolder")
     .parse(process.argv);
 
 (async () => {
@@ -17,11 +19,17 @@ program
     //advertisement
     console.log("> InstaHooker CLI - By Bilal Taner (shynox)\n> Thanks to Tuhana (tuhana) for helping cli\n> Type -h for help menu");
 
-    if (!options.username)
-        return console.log("You must enter a username!")
-    else if (!options.sid)
+    if (!options.sid)
         return console.log("You must enter a sessionid for hooking images!")
+
+    if (options.post && !options.u) {
+        return await PostHooker({ url: options.post, cookie: options.sid, timeout: options?.timeout, output: options?.output });
+    }
+
+    if (!options.username)
+        return console.log("You must enter a username!");
+
     else {
-        await startHook({ username: options.username, cookie: options.sid, timeout: options?.timeout, output: options?.output })
+        return await ProfileHooker({ username: options.username, cookie: options.sid, timeout: options?.timeout, output: options?.output });
     }
 })();
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const fs = require("fs");
 const fetch = require("node-fetch");
 
-module.exports.startHook = async (setting = { username: "", cookie: "", timeout: 50, output: "" }) => {
+const baseURL = `https://www.instagram.com/`;
+const baseURLQuery = `https://www.instagram.com/graphql/query/?query_hash=8c2a529969ee035a5063f2fc8602a0fd&variables=`;
+
+const postRegex = /(https?:\/\/(?:www\.)?instagram\.com\/(p|reel|tv)\/([^\/?#&]+)).*/;
+
+module.exports.ProfileHooker = async (setting = { username: "", cookie: "", timeout: 50, output: "" }) => {
     const options = {
         username: setting.username,
         cookie: `sessionid=${setting.cookie};`,
@@ -11,12 +16,12 @@ module.exports.startHook = async (setting = { username: "", cookie: "", timeout:
 
     const posts = [];
 
-    fetch(`https://www.instagram.com/${options.username}/?__a=1`, { headers: { cookie: options.cookie } }).then(async response => {
+    fetch(`${baseURL}${options.username}/?__a=1`, { headers: { cookie: options.cookie } }).then(async response => {
         const data = await response.json();
         if (!data.graphql) return console.log("WARN: User not found!");
 
         const id = data.graphql.user.id,
-            fetchedData = await fetch(`https://www.instagram.com/graphql/query/?query_hash=8c2a529969ee035a5063f2fc8602a0fd&variables={"id":"${id}","first":"5000"}`, { headers: { cookie: options.cookie } }).then(res => res.json()),
+            fetchedData = await fetch(`${baseURLQuery}{"id":"${id}","first":"5000"}`, { headers: { cookie: options.cookie } }).then(res => res.json()),
             mediaCount = fetchedData.data.user.edge_owner_to_timeline_media.count;
 
         if (!mediaCount) return console.log("WARN: There are no posts on the account you entered!");
@@ -33,7 +38,7 @@ module.exports.startHook = async (setting = { username: "", cookie: "", timeout:
         if (mediaCount > 50) {
             for (let i = 0; i < Math.floor(mediaCount / 50); i++) {
 
-                const fetchingMediaFor = await fetch(`https://www.instagram.com/graphql/query/?query_hash=8c2a529969ee035a5063f2fc8602a0fd&variables={"id":"${id}","first":"5000","after":"${after}"}`, { headers: { cookie: options.cookie } })
+                const fetchingMediaFor = await fetch(`${baseURLQuery}{"id":"${id}","first":"5000","after":"${after}"}`, { headers: { cookie: options.cookie } })
                     .then(x => x.json());
 
                 getData(fetchingMediaFor, posts);
@@ -55,7 +60,34 @@ module.exports.startHook = async (setting = { username: "", cookie: "", timeout:
         await sleep(1300);
         await getRemoteFile(posts, `./${options.output}/`, options.timeout);
     })
+}
 
+
+module.exports.PostHooker = async (setting = { url: "", cookie: "", timeout: 50, output: "" }) => {
+    const options = {
+        url: setting.url,
+        cookie: `sessionid=${setting.cookie};`,
+        timeout: setting.timeout ? setting.timeout : 50,
+        output: setting.output || "instahooker-out"
+    }
+
+    const posts = [];
+
+    const data = await fetch(`${baseURL}/p/${options.url.startsWith(baseURL) ? postRegex.exec(options.url)[3] : options.url}/?__a=1`, { headers: { cookie: options.cookie } })
+        .then(res => res.json());
+
+    getData(data, posts, "post");
+
+    //Check the posts array
+    if (!posts.length && data.graphql.user.is_private) return console.log("WARN: The account you want to reach is private and you don't follow it, so media cannot be downloaded!");
+
+    //Show info for download
+    await sleep(300);
+    console.log("READY: All contents have been fetched, preparing to download.");
+    await sleep(300);
+    console.log("READY: Download Starting...");
+    await sleep(800);
+    await getRemoteFile(posts, `./${options.output}/`, options.timeout);
 
 }
 
@@ -65,53 +97,84 @@ async function getRemoteFile(PostArray, output, ms) {
             recursive: true
         });
     }
-    let i = 0;
-    PostArray.filter(x => x.url.startsWith("https://instagram")).forEach(async (x) => {
-        await fetch(x.url).then(res => res.buffer()).then(async image => {
 
+    let i = 0;
+
+    //i am not using forEach cuz sleep function is not working when i use forEach :(
+    for (const x of PostArray.filter(x => x.url.startsWith("https://instagram"))) {
+        await fetch(x.url).then(res => res.buffer()).then(async image => {
             fs.writeFileSync(output + x.name, image, (error) => { console.log(error) });
             process.stdout.write(`\rINFO: Media "${x.name}" downloaded. PROGRESS: (${i + 1}/${PostArray.length})`);
 
         });
 
-        i++;
         await sleep(ms);
+        i++;
 
         if (i === PostArray.length) {
             return console.log(`\nCOMPLETE: Download finished! Statics: Video Count: ${PostArray.filter(x => x.name.endsWith(".mp4")).length} - Photo Count: ${PostArray.filter(x => x.name.endsWith(".png")).length}`)
                 , console.log("> InstaHooker CLI - By Bilal Taner (shynox)\n> Thanks to Tuhana (tuhana) for helping cli\n> Type -h for help menu");
         };
-    });
+    }
 }
 
-function getData(fetch, posts) {
-    for (const media of fetch.data.user.edge_owner_to_timeline_media.edges) {
-        const node = media.node;
-        if (!node.__typename) continue;
+function getData(fetch, posts, type) {
 
-        switch (node.__typename) {
-            case "GraphSidecar":
-                let i = 0;
-                for (let sidecar of node.edge_sidecar_to_children.edges) {
-                    if (sidecar.node.video_url) {
-                        posts.push({ name: `instaHooker_${node.shortcode}_${i}_${node.id}.mp4`, url: sidecar.node.video_url });
-                    } else {
-                        posts.push({ name: `instaHooker_${node.shortcode}_${i}_${node.id}.png`, url: sidecar.node.display_url });
-                    }
-                    i++;
+    switch (type) {
+        case "post":
+            const node = fetch.graphql.shortcode_media;
+            switch (fetch.graphql.shortcode_media.__typename) {
+                case "GraphSidecar":
+                    let i = 0;
+                    node.edge_sidecar_to_children.edges.forEach(sidecar => {
+                        if (sidecar.node.video_url) {
+                            posts.push({ name: `instaHooker_${node.shortcode}_${i}_${node.id}.mp4`, url: sidecar.node.video_url });
+                        } else {
+                            posts.push({ name: `instaHooker_${node.shortcode}_${i}_${node.id}.png`, url: sidecar.node.display_url });
+                        }
+                        i++;
+                    })
+                    break;
+
+                case "GraphVideo":
+                    posts.push({ name: `instaHooker_${node.shortcode}_0_${node.id}.mp4`, url: node.video_url });
+                    break;
+
+                case "GraphImage":
+                    if (!node.edge_sidecar_to_children)
+                        posts.push({ name: `instaHooker_${node.shortcode}_0_${node.id}.png`, url: node.display_url });
+            }
+            break;
+
+        default:
+            fetch.data.user.edge_owner_to_timeline_media.edges.forEach(x => {
+                const node = x.node;
+                if (!node.__typename) return;
+
+                switch (node.__typename) {
+                    case "GraphSidecar":
+                        let i = 0;
+                        node.edge_sidecar_to_children.edges.forEach(sidecar => {
+                            if (sidecar.node.video_url) {
+                                posts.push({ name: `instaHooker_${node.shortcode}_${i}_${node.id}.mp4`, url: sidecar.node.video_url });
+                            } else {
+                                posts.push({ name: `instaHooker_${node.shortcode}_${i}_${node.id}.png`, url: sidecar.node.display_url });
+                            }
+                            i++;
+                        })
+                        break;
+
+                    case "GraphVideo":
+                        posts.push({ name: `instaHooker_${node.shortcode}_0_${node.id}.mp4`, url: node.video_url });
+                        break;
+
+                    case "GraphImage":
+                        if (!node.edge_sidecar_to_children)
+                            posts.push({ name: `instaHooker_${node.shortcode}_0_${node.id}.png`, url: node.display_url });
+
+
                 }
-                break;
-
-            case "GraphVideo":
-                posts.push({ name: `instaHooker_${node.shortcode}_0_${node.id}.mp4`, url: node.video_url });
-                break;
-
-            case "GraphImage":
-                if (!node.edge_sidecar_to_children) {
-                    posts.push({ name: `instaHooker_${node.shortcode}_0_${node.id}.png`, url: node.display_url });
-                }
-
-        }
+            })
     }
 }
 
