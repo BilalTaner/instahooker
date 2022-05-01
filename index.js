7b178a18186e9058b6e40aef7984af58ae0e7bef
/shynox <66389292+BilalTaner@users.noreply.github.com>
2022-05-01 18:42:33 +0300

    new version and added index selector

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const postRegex = /(https?:\/\/(?:www\.)?instagram\.com\/(p|reel|tv)\/([^\/?#&]+
     storyRegex = /https?:\/\/(?:www\.)?instagram\.com\/stories\/(.*?)\//,
     profileRegex = /(?<=instagram.com\/)[A-Za-z0-9_.]+/igm;
 
-<<<<<<< HEAD
 class InstagramDumper {
 
     /**
@@ -35,29 +34,6 @@ class InstagramDumper {
             "user-agent": 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)'
         }
     }
-=======
-module.exports.ProfileHooker = async (settings = { username: "", cookie: "", timeout: 20, output: "" }) => {
-    const options = {
-        username: settings.username,
-        cookie: `sessionid=${settings.cookie};`,
-        timeout: settings.timeout ? settings.timeout : 20,
-        output: settings.output
-    }
-
-    const posts = [];
-
-    fetch(`${baseURL}${options.username.startsWith(baseURL) ? profileRegex.exec(options.username)[0] : options.username}/?__a=1`, { headers: { cookie: options.cookie } }).then(async response => {
-        const data = await response.json();
-
-        if (!data.graphql) return logger("warn", "User not found!");
-
-        const id = data.graphql.user.id,
-            fetchedData = await fetch(`${baseURLQuery}{"id":"${id}","first":"5000"}`, { headers: { cookie: options.cookie } }).then(res => res.json()),
-            mediaCount = fetchedData.data.user.edge_owner_to_timeline_media.count;
-
-        if (!mediaCount)
-            return logger("warn", "There are no posts on the account you entered!");
->>>>>>> 17fe42da5abea863f755b72f73da8164ab32d684
 
     /**
      * 
@@ -112,7 +88,6 @@ module.exports.ProfileHooker = async (settings = { username: "", cookie: "", tim
     }
 
 
-<<<<<<< HEAD
     /**
      * 
      * @param {any[]} ImageArray 
@@ -127,10 +102,6 @@ module.exports.ProfileHooker = async (settings = { username: "", cookie: "", tim
         }
 
         let i = 0;
-=======
-        if (mediaCount > 50) {
-            for (let i = 0; i < Math.floor(mediaCount / 50); i++) {
->>>>>>> 17fe42da5abea863f755b72f73da8164ab32d684
 
         let mainArray = (this.index && this.index > 0 && ImageArray.length >= this.index ? [ImageArray[this.index - 1]] : ImageArray)
 
@@ -150,7 +121,6 @@ module.exports.ProfileHooker = async (settings = { username: "", cookie: "", tim
         }
     }
 
-<<<<<<< HEAD
     /**
      * 
      * @param {any} fetch 
@@ -171,21 +141,6 @@ module.exports.ProfileHooker = async (settings = { username: "", cookie: "", tim
                     }
                 });
                 break;
-=======
-        //Check the posts array
-        if (!posts.length && data.graphql.user.is_private)
-            return logger("warn", "The account you want to reach is private and you don't follow it, so media cannot be downloaded!");
-
-        //Show info for download (when i use logger func its broken)
-        await sleep(500);
-        console.log("\nREADY: All contents have been fetched, preparing to download.");
-        await sleep(500);
-        console.log("READY: Download Starting...");
-        await sleep(800);
-        await getRemoteFile(posts, `./${options.output || data.graphql.user.username}/instahooker-profile/`, options.timeout);
-    })
-}
->>>>>>> 17fe42da5abea863f755b72f73da8164ab32d684
 
             case "post":
                 const node = fetch.items[0];
@@ -209,7 +164,6 @@ module.exports.ProfileHooker = async (settings = { username: "", cookie: "", tim
                 }
                 break;
 
-<<<<<<< HEAD
             default:
                 fetch.data.user.edge_owner_to_timeline_media.edges.forEach(x => {
                     const node = x.node;
@@ -238,14 +192,6 @@ module.exports.ProfileHooker = async (settings = { username: "", cookie: "", tim
                     }
                 })
         }
-=======
-module.exports.PostHooker = async (settings = { url: "", cookie: "", timeout: 20, output: "" }) => {
-    const options = {
-        url: settings.url,
-        cookie: `sessionid=${settings.cookie};`,
-        timeout: settings.timeout ? settings.timeout : 20,
-        output: settings.output || "instahooker-out"
->>>>>>> 17fe42da5abea863f755b72f73da8164ab32d684
     }
 
     async ProfileDumper() {
@@ -259,34 +205,16 @@ module.exports.PostHooker = async (settings = { url: "", cookie: "", timeout: 20
                 fetchedData = await fetch(`${baseURLQuery}{"id":"${id}","first":"5000"}`, { headers: this.getHeader() }).then(res => res.json()),
                 mediaCount = fetchedData.data.user.edge_owner_to_timeline_media.count;
 
-<<<<<<< HEAD
             if (!mediaCount)
                 return await this.log("warnContentNotFound");
-=======
-    //Show info for download
-    await logger("ready");
-    await getRemoteFile(posts, `./${options.output || data.user.username}/instahooker-posts/`, options.timeout);
->>>>>>> 17fe42da5abea863f755b72f73da8164ab32d684
 
             let after = fetchedData.data.user.edge_owner_to_timeline_media.page_info.end_cursor;
 
             this.getData(fetchedData, posts);
 
-<<<<<<< HEAD
             await this.log("fetchReady", 200);
 
             process.stdout.write(`\r🔘 READY: Fetcing data from instagram... PROGRESS: [ ${mediaCount > 50 ? 50 : mediaCount}/${mediaCount} (${posts.length} media) ]`);
-=======
-module.exports.StoryHooker = async (settings = { username: "", cookie: "", timeout: 20, output: "", highlight: false }) => {
-
-    const options = {
-        username: settings.username,
-        cookie: `sessionid=${settings.cookie};`,
-        timeout: settings.timeout ? settings.timeout : 20,
-        output: settings.output,
-        highlight: settings.highlight || false
-    }
->>>>>>> 17fe42da5abea863f755b72f73da8164ab32d684
 
             if (mediaCount > 50) {
                 for (let i = 0; i < Math.floor(mediaCount / 50); i++) {
@@ -296,13 +224,9 @@ module.exports.StoryHooker = async (settings = { username: "", cookie: "", timeo
                     const fetchingMediaFor = await fetch(`${baseURLQuery}{"id":"${id}","first":"5000","after":"${after}"}`, { headers: this.getHeader() })
                         .then(x => x.json());
 
-<<<<<<< HEAD
                     this.getData(fetchingMediaFor, posts);
 
                     process.stdout.write(`\r🔘 READY: Fetcing data from instagram... PROGRESS: [ ${i + 1 >= Math.floor(mediaCount / 50) ? mediaCount : (i + 2) * 50}/${mediaCount} (${posts.length} media) ]`);
-=======
-        await logger("fetchReady");
->>>>>>> 17fe42da5abea863f755b72f73da8164ab32d684
 
                     after = fetchingMediaFor.data.user.edge_owner_to_timeline_media.page_info.end_cursor;
                 }
@@ -325,24 +249,12 @@ module.exports.StoryHooker = async (settings = { username: "", cookie: "", timeo
      */
     async StoryHighlightDumper(isHighlight) {
 
-<<<<<<< HEAD
-=======
-        //Show info for download
-        await logger("ready");
-        return await getRemoteFile(posts, `./${options.output || Object.values(highlightStories.reels)[0].user.username}/instahooker-highlight/`, options.timeout);
->>>>>>> 17fe42da5abea863f755b72f73da8164ab32d684
 
         if (isHighlight || this.input.startsWith("https://www.instagram.com/stories/highlights/")) {
 
             await this.log("fetchReady", 200);
 
-<<<<<<< HEAD
             const highlightStories = await fetch(`${baseURLFeed}reels_media/?reel_ids=highlight:${this.input.startsWith("https://www.instagram.com/stories/highlights/") ? highlightRegex.exec(this.input)[1] : this.input}`, { headers: this.getHeader() }).then(res => res.json());
-=======
-        await fetch(`${baseURL}${options.username.startsWith("https://www.instagram.com/stories/") ? storyRegex.exec(options.username)[1] : options.username.startsWith(baseURL) ? profileRegex.exec(options.username)[0] : options.username}/?__a=1`, { headers: { cookie: options.cookie } }).then(async response => {
-            const data = await response.json();
-            if (!data.graphql) return logger("warn", "User not found!")
->>>>>>> 17fe42da5abea863f755b72f73da8164ab32d684
 
             this.getData(Object.values(highlightStories.reels)[0], posts, "story");
 
@@ -361,25 +273,7 @@ module.exports.StoryHooker = async (settings = { username: "", cookie: "", timeo
                 const data = await response.json();
                 if (!data.graphql) return this.log("warnUserNotFound");
 
-<<<<<<< HEAD
                 const id = data.graphql.user.id;
-=======
-                //Checking array for story is exits
-                if (!posts.length) {
-                    if (data.graphql.user.is_private) {
-                        return logger("warn", "The account you want to reach is private and you don't follow it, so media cannot be downloaded!");
-                    }
-                    return logger("warn", "There are no stories on the account you entered!")
-                }
-
-                //Show info for download
-                await logger("ready");
-                return await getRemoteFile(posts, `./${options.output || data.graphql.user.username}/instahooker-story/`, options.timeout);
-            })
-        });
-    }
-}
->>>>>>> 17fe42da5abea863f755b72f73da8164ab32d684
 
                 await fetch(`${baseURLFeed}reels_tray/`, { headers: this.getHeader() }).then(async () => {
 
@@ -412,73 +306,7 @@ module.exports.StoryHooker = async (settings = { username: "", cookie: "", timeo
         const data = await fetch(`${baseURL}/p/${this.input.startsWith(baseURL) ? postRegex.exec(this.input)[3] : this.input}/?__a=1`, { headers: this.getHeader() })
             .then(res => res.json());
 
-<<<<<<< HEAD
         this.getData(data, posts, "post");
-=======
-function getData(fetch, posts, type) {
-
-    switch (type) {
-        case "story":
-            fetch.items.forEach((x) => {
-                switch (x.media_type) {
-                    case 1:
-                        posts.push({ name: `instaHookerStory_${x.pk}.png`, url: x.image_versions2.candidates[0].url });
-                        break;
-                    case 2:
-                        posts.push({ name: `instaHookerStory_${x.pk}.mp4`, url: x.video_versions[0].url });
-                        break;
-                }
-            });
-            break;
-
-        case "post":
-            const node = fetch.items[0];
-            if (node.carousel_media) {
-                let i = 0;
-                node.carousel_media.forEach(sidecar => {
-                    if (sidecar.media_type === 2) {
-                        posts.push({ name: `instaHooker_${sidecar.code}_${i}_${node.pk}.mp4`, url: sidecar.video_versions[0].url });
-                    } else {
-                        posts.push({ name: `instaHooker_${sidecar.code}_${i}_${node.pk}.png`, url: sidecar.image_versions2.candidates[0].url });
-                    }
-                    i++;
-                })
-                break;
-            } else {
-                if (node.video_versions) {
-                    posts.push({ name: `instaHooker_${node.code}_0_${node.pk}.mp4`, url: node.video_versions[0].url });
-                } else if (node.image_versions2.candidates[0]) {
-                    posts.push({ name: `instaHooker_${node.code}_0_${node.pk}.png`, url: node.image_versions2.candidates[0].url });
-                }
-            }
-            break;
-
-        default:
-            fetch.data.user.edge_owner_to_timeline_media.edges.forEach(x => {
-                const node = x.node;
-                if (!node.__typename) return;
-
-                switch (node.__typename) {
-                    case "GraphSidecar":
-                        let i = 0;
-                        node.edge_sidecar_to_children.edges.forEach(sidecar => {
-                            if (sidecar.node.video_url) {
-                                posts.push({ name: `instaHooker_${node.shortcode}_${i}_${node.id}.mp4`, url: sidecar.node.video_url });
-                            } else {
-                                posts.push({ name: `instaHooker_${node.shortcode}_${i}_${node.id}.png`, url: sidecar.node.display_url });
-                            }
-                            i++;
-                        })
-                        break;
-
-                    case "GraphVideo":
-                        posts.push({ name: `instaHooker_${node.shortcode}_0_${node.id}.mp4`, url: node.video_url });
-                        break;
-
-                    case "GraphImage":
-                        if (!node.edge_sidecar_to_children)
-                            posts.push({ name: `instaHooker_${node.shortcode}_0_${node.id}.png`, url: node.display_url });
->>>>>>> 17fe42da5abea863f755b72f73da8164ab32d684
 
         //Check the posts array
         if (!posts.length && data.items[0].user.is_private)
@@ -496,10 +324,4 @@ process.on("unhandledRejection", () => {
     return console.log("❌ ERROR: Something went wrong. Please recheck the values you entered! (Suggestion: Check your cookie first.)");
 });
 
-<<<<<<< HEAD
 module.exports = InstagramDumper;
-=======
-process.on("unhandledRejection", (error) => {
-    return console.log("ERROR: Something went wrong. Please recheck the values ​​you entered");
-});
->>>>>>> 17fe42da5abea863f755b72f73da8164ab32d684
